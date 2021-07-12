700647a0afa48bb9757a112884fd599a867bb877
Brett Saiki <bksaiki@gmail.com>
2021-07-12 16:56:18 -0700

    Bump Herbie version

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define name "fixedpoint-herbie")
-(define deps '("math-lib" "base" ("herbie" #:version "1.4")))
+(define deps '("math-lib" "base" ("herbie" #:version "1.5")))
 (define pkg-desc "Herbie plugin for fixed-point numbers")
 (define version "1.0")
 (define pkg-authors '("Brett Saiki"))
